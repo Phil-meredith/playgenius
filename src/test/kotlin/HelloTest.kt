240876010aa9856387be16b70a2d0a842018351f
shada <phillip.meredith@grimnirtech.com>
2019-04-01 13:09:14 +0100

    test hello route response

--- src/test/kotlin/HelloTest.kt
+++ src/test/kotlin/HelloTest.kt
@@ -1,11 +1,14 @@
+import app.routes
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import org.http4k.core.Method
 import org.junit.Test
 
 class HelloTest {
 
     @Test
-    fun test(){
-        assertThat(1, equalTo(1))
+    fun routeTest(){
+        val response = routes.invoke(org.http4k.core.Request(Method.GET, "/hello?name=bob"))
+        assertThat(response.bodyString(), equalTo("Hello, bob!"))
     }
 }
