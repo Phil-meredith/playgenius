748dc874edcd051fe658cc37a4812ac821201d90
shada <phillip.meredith@grimnirtech.com>
2019-04-01 15:27:21 +0100

    add landing page

--- src/main/kotlin/app/App.kt
+++ src/main/kotlin/app/App.kt
@@ -1,7 +1,6 @@
 package app
 
-import org.http4k.core.HttpHandler
-import org.http4k.core.Method.GET
+import org.http4k.core.*
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
@@ -12,7 +11,9 @@ fun main(args: Array<String>) {
     routes.asServer(Jetty(port)).start()
 }
 
-val routes: HttpHandler = routes("/hello" bind GET to { request: org.http4k.core.Request ->
-    org.http4k.core.Response(org.http4k.core.Status.Companion.OK)
-        .body("Hello, ${request.query("name")}!")
-})
+val routes: HttpHandler = routes(
+    "/hello" bind Method.GET to { request ->
+    Response(Status.OK)
+        .body("Hello, ${request.query("name")}!")},
+    "/" bind Method.GET to {Response(Status.OK).body("Play Smarter, Play harder, Play Genius")}
+)
