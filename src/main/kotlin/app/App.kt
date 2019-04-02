129db985f034b00fef03c455f93f001ea67b6ba6
shada <phillip.meredith@grimnirtech.com>
2019-04-02 11:12:37 +0100

    add scss build for heroku

--- src/main/kotlin/app/App.kt
+++ src/main/kotlin/app/App.kt
@@ -7,17 +7,29 @@ import org.http4k.server.Jetty
 import org.http4k.server.asServer
 import org.http4k.core.Method.GET
 import org.http4k.core.Status.Companion.OK
+import org.http4k.filter.ResponseFilters
+import org.http4k.routing.path
+import java.time.Clock
 
 fun main(args: Array<String>) {
+    fun logger(message: String) = println("${Clock.systemUTC().instant()} $message")
+
+    val audit = ResponseFilters.ReportHttpTransaction { tx: HttpTransaction ->
+        logger("Call to ${tx.request.uri} returned ${tx.response.status} and took ${tx.duration.toMillis()}")
+    }
+
     val port = if (args.isNotEmpty()) args[0].toInt() else 9000
-    Routes.routes.asServer(Jetty(port)).start()
+    audit.then(Routes.routes).asServer(Jetty(port)).start()
 }
 
 
 object Routes {
     val routes: HttpHandler = routes(
         "/hello" bind GET to { req -> Response(OK).body("Hello, ${req.query("name")}!") },
-        "/" bind GET to { Response(OK).header("content-type","text/html")
-            .body(String(this.javaClass.getResourceAsStream("/public/html/index.html").readBytes())) }
+        "/{assetType}/{fileName}" bind GET to { req -> loadResponse("text/${req.path("assetType")}", "/public/${req.path("assetType")}/${req.path("fileName")}") },
+        "/" bind GET to {loadResponse("text/html", "/public/html/index.html")}
     )
+
+    private fun loadResponse(contentType: String, location: String) = Response(OK).header("content-type", contentType)
+        .body(String(this.javaClass.getResourceAsStream(location).readBytes()))
 }
--- src/test/kotlin/HelloTest.kt
+++ src/test/kotlin/HelloTest.kt
@@ -3,12 +3,14 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.http4k.core.Method
 import org.junit.Test
+import org.http4k.core.Request
+import org.http4k.core.Status
 
 class HelloTest {
 
     @Test
     fun routeTest(){
-        val response = routes.invoke(org.http4k.core.Request(Method.GET, "/hello?name=bob"))
+        val response = routes.invoke(Request(Method.GET, "/hello?name=bob"))
         assertThat(response.bodyString(), equalTo("Hello, bob!"))
     }
 }
