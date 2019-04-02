42bb210262ac30961c8393d1857d8531d0b21bec
shada <phillip.meredith@grimnirtech.com>
2019-04-02 14:01:54 +0100

    Basic splash page
    proves out using sass and images

--- src/main/kotlin/app/App.kt
+++ src/main/kotlin/app/App.kt
@@ -9,16 +9,17 @@ import org.http4k.core.Method.GET
 import org.http4k.core.Status.Companion.OK
 import org.http4k.filter.ResponseFilters
 import org.http4k.routing.path
+import java.nio.ByteBuffer
 import java.time.Clock
 
 fun main(args: Array<String>) {
+    val port = if (args.isNotEmpty()) args[0].toInt() else 9000
     fun logger(message: String) = println("${Clock.systemUTC().instant()} $message")
 
     val audit = ResponseFilters.ReportHttpTransaction { tx: HttpTransaction ->
         logger("Call to ${tx.request.uri} returned ${tx.response.status} and took ${tx.duration.toMillis()}")
     }
 
-    val port = if (args.isNotEmpty()) args[0].toInt() else 9000
     audit.then(Routes.routes).asServer(Jetty(port)).start()
 }
 
@@ -26,10 +27,14 @@ fun main(args: Array<String>) {
 object Routes {
     val routes: HttpHandler = routes(
         "/hello" bind GET to { req -> Response(OK).body("Hello, ${req.query("name")}!") },
-        "/{assetType}/{fileName}" bind GET to { req -> loadResponse("text/${req.path("assetType")}", "/public/${req.path("assetType")}/${req.path("fileName")}") },
-        "/" bind GET to {loadResponse("text/html", "/public/html/index.html")}
+        "/{assetType}/{fileName}" bind GET to { req -> assetResponse(req.path("assetType"), req.path("fileName")) },
+        "/" bind GET to { assetResponse("html", "index.html") }
     )
 
-    private fun loadResponse(contentType: String, location: String) = Response(OK).header("content-type", contentType)
-        .body(String(this.javaClass.getResourceAsStream(location).readBytes()))
+    private fun assetResponse(assetType: String?, fileName: String?) =
+        Response(OK).header("content-type", if(assetType == "image") "image/jpeg" else "text/$assetType").withBody(assetType, fileName)
+
+    private fun Response.withBody(assetType: String?, fileName: String?): Response =
+        if (assetType == "image") body(Body(ByteBuffer.wrap(this.javaClass.getResourceAsStream("/public/image/$fileName").readBytes())))
+        else body(String(this.javaClass.getResourceAsStream("/public/$assetType/$fileName").readBytes()))
 }
